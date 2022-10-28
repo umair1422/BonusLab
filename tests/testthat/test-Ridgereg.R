990d3fc6c03771cea5bb17d0d9dab27552c822e3
umair <umair.raja.1422@gmail.com>
2022-10-29 03:26:54 +0500

    code and tests added

---
+++ tests/testthat/test-Ridgereg.R
@@ -0,0 +1,16 @@
+testthat::test_that("correct result",{
+  library(MASS)
+  testthat::expect_equal(ridgereg(Petal.Length~Species, data =iris,lam=3),lm.ridge(Petal.Length~Species, data =iris,lam=3))
+  testthat::expect_success(ridgereg(Petal.Length~Species, data =iris,lam=3))
+  testthat::expect_success(ridgereg(Petal.Length~Species, data =iris,lam=4))
+
+})
+
+testthat::test_that("Wrong results",{
+  testthat::expect_error(ridgereg(formula,iris,a))
+  testthat::expect_error(ridgereg(formula,iris,"a"))
+  testthat::expect_error(ridgereg(formula,iris,TRUE))
+  testthat::expect_error(ridgereg(formula,hello,3))
+
+})
+
