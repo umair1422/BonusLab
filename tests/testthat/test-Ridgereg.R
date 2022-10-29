aa9dad191c876f24b1dbb1d7c877d3ccbd64de74
Usama Nadeem <usamanadeem321@gmail.com>
2022-10-29 16:58:14 +0200

    tests updated

--- tests/testthat/test-Ridgereg.R
+++ tests/testthat/test-Ridgereg.R
@@ -1,16 +1,8 @@
-testthat::test_that("correct result",{
-  library(MASS)
-  testthat::expect_success(ridgereg(Petal.Length~Species, data =iris,lam=3))
-  testthat::expect_success(ridgereg(Petal.Length~Species, data =iris,lam=4))
-
-})
-
-
 testthat::test_that("Wrong results",{
-  testthat::expect_error(ridgereg(formula,iris,a))
-  testthat::expect_error(ridgereg(formula,iris,"a"))
-  testthat::expect_error(ridgereg(formula,iris,TRUE))
-  testthat::expect_error(ridgereg(formula,hello,3))
+  testthat::expect_error(Ridgereg(formula,iris,a))
+  testthat::expect_error(Ridgereg(formula,iris,"a"))
+  testthat::expect_error(Ridgereg(formula,iris,TRUE))
+  testthat::expect_error(Ridgereg(formula,hello,3))
 
 })
 
